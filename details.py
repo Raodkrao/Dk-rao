d586aad85eb02e89e861fcd175866697c46cf93d
Raodkrao <133095873+Raodkrao@users.noreply.github.com>
2023-09-05 21:30:41 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 23765127
-api_hash = "f55cf6fc4498f1fbcecf2ec3c074c9dc"
-bot_token = "6184305721:AAGhd2Ei4QqFuE2gh-QXMUochKqqb5D75vk"
-auth_users = [6073137039,-1001941110126]
-sudo_user = 6073137039
-log_channel = -1001941110126
-txt_channel = -1001941110126
+api_id = 22896344
+api_hash = "c8c80b086cae29bf1507f70312f60379"
+bot_token = "6606753808:AAFzXRegdnzt4M2rKXZ5YfaJvIkib_RPWVk"
+auth_users = [6120099117,-1001845392899]
+sudo_user = 6120099117
+log_channel = -1001845392899
+txt_channel = -1001845392899
